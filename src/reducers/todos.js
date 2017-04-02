ff2f694f457bda1dba23c16955e177ce82520f88
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2017-04-02 22:13:27 +0100

    19

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,42 +1,62 @@
 import { combineReducers } from 'redux'
-import todo from './todo'
 
 const byId = (state = {}, action) => {
   switch (action.type) {
-    case 'ADD_TODO':
-    case 'TOGGLE_TODO':
-      return {...state, [action.id]: todo(state[action.id], action)}
+    case 'RECEIVE_TODOS':
+      const nextState = {...state}
+      action.response.forEach(todo => { nextState[todo.id] = todo })
+      return nextState
     default:
       return state
   }
 }
 
 const allIds = (state = [], action) => {
+  if (action.filter !== 'all') return state
+
   switch (action.type) {
-    case 'ADD_TODO':
-      return [...state, action.id]
+    case 'RECEIVE_TODOS':
+      return action.response.map(todo => todo.id)
     default:
       return state
   }
 }
 
-const todos = combineReducers({byId, allIds})
+const activeIds = (state = [], action) => {
+  if (action.filter !== 'active') return state
+
+  switch (action.type) {
+    case 'RECEIVE_TODOS':
+      return action.response.map(todo => todo.id)
+    default:
+      return state
+  }
+}
+
+const completedIds = (state = [], action) => {
+  if (action.filter !== 'completed') return state
+
+  switch (action.type) {
+    case 'RECEIVE_TODOS':
+      return action.response.map(todo => todo.id)
+    default:
+      return state
+  }
+}
+
+const idsByFilter = combineReducers({
+  all: allIds,
+  active: activeIds,
+  completed: completedIds
+})
+
+const todos = combineReducers({byId, idsByFilter})
 
 export default todos
 
-const getAllTodos = (state) => state.allIds.map(id => state.byId[id])
 
 export const getVisibleTodos = (state, filter) => {
-  const allTodos = getAllTodos(state)
+  const ids = state.idsByFilter[filter]
 
-  switch (filter) {
-    case 'all':
-      return allTodos
-    case 'completed':
-      return allTodos.filter(t => t.completed)
-    case 'active':
-      return allTodos.filter(t => !t.completed)
-    default:
-      throw new Error(`Unknown filter: ${filter}`)
-  }
+  return ids.map(id => state.byId[id])
 }
