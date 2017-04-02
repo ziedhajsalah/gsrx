40af09dca42cf7aca6d7325ba726aeab00f7f6f5
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2017-04-02 18:35:04 +0100

    18

--- src/lib/configureStore.js
+++ src/lib/configureStore.js
@@ -1,44 +1,17 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import promise from 'redux-promise'
+import logger from 'redux-logger'
 import todoApp from '../reducers'
 
-const logger = store => next => {
-  if (!console.group) return next
-
-  return action => {
-    console.group(action.type)
-    console.log('%c prev state: ', 'color: gray', store.getState())
-    console.log('%c action: ', 'color: blue', action)
-    const returnValue = next(action)
-    console.log('%c next state: ', 'color: green', store.getState())
-    console.groupEnd(action.type)
-
-    return returnValue
-  }
-}
-
-const promise = store => next => action => {
-  if (typeof action.then === 'function') {
-    return action.then(next)
-  }
-
-  return next(action)
-}
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(middleware => {
-    store.dispatch = middleware(store)(store.dispatch)
-  })
-}
-
 export default function configureStore () {
-  const store = createStore(todoApp)
   const middlewares = [promise]
 
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(logger)
   }
 
-  wrapDispatchWithMiddlewares(store, middlewares)
-
-  return store
+  return createStore(
+    todoApp,
+    applyMiddleware(...middlewares)
+  )
 }
