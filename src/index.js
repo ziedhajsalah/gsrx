996d12170331ccd3dda17201795058a53b6ab5d1
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2017-04-02 12:36:59 +0100

    12

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { configureStore } from './lib/configureStore.js'
+import configureStore from './lib/configureStore.js'
 import Root from './components/Root'
 
 const store = configureStore()
--- src/lib/configureStore.js
+++ src/lib/configureStore.js
@@ -3,9 +3,30 @@ import throttle from 'lodash/throttle'
 import todoApp from '../reducers'
 import { saveState, loadState } from './localStorage'
 
-export function configureStore () {
+const addLoggingToDispatch = store => {
+  const rawDispatch = store.dispatch
+
+  if (!console.group) return rawDispatch
+
+  return action => {
+    console.group(action.type)
+    console.log('%c prev state: ', 'color: gray', store.getState())
+    console.log('%c action: ', 'color: blue', action)
+    const returnValue = rawDispatch(action)
+    console.log('%c next state: ', 'color: green', store.getState())
+    console.group(action.type)
+
+    return returnValue
+  }
+}
+
+export default function configureStore () {
   const persistedState = loadState()
   const store = createStore(todoApp, persistedState)
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store)
+  }
 
   store.subscribe(throttle(() => {
     saveState({todos: store.getState().todos})
